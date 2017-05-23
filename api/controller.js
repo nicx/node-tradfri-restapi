29d3d214f0fd95ead513f2afdcd546425c927e18
nidayand <pggithub@gothager.se>
2017-05-23 23:14:30 +0200

    Error handling

--- api/controller.js
+++ api/controller.js
@@ -5,54 +5,118 @@ const tradfri = require('node-tradfri').create({
     coapClientPath: config.coapClientPath,
     securityId: config.securityId,
     hubIpAddress: config.ip
-  });
+});
 
-exports.getDeviceIds = function(req,res){
-   //console.log(req);
-    tradfri.getDeviceIds().then(deviceIds => {
-        res.json({items: deviceIds, status:"ok"});
-    });
+exports.getDeviceIds = function (req, res) {
+    try {
+        tradfri.getDeviceIds().then(deviceIds => {
+            res.json({
+                items: deviceIds,
+                status: "ok"
+            });
+        });
+
+    } catch (err) {
+        res.json({
+            items: [],
+            status: err
+        });
+    }
 }
 
-exports.getDevices = function(req,res){
-   //console.log(req);
-    tradfri.getDevices().then(devices => {
-        res.json({items: devices, status:"ok"});
-    });
+exports.getDevices = function (req, res) {
+    try {
+        tradfri.getDevices().then(devices => {
+            res.json({
+                items: devices,
+                status: "ok"
+            });
+        });
+
+    } catch (err) {
+        res.json({
+            items: [],
+            status: err
+        });
+    }
 }
-exports.getGroupIds = function(req,res){
-   //console.log(req);
-    tradfri.getGroupIds().then(groupIds => {
-        res.json({items: groupIds, status:"ok"});
-    });
+exports.getGroupIds = function (req, res) {
+    try {
+        tradfri.getGroupIds().then(groupIds => {
+            res.json({
+                items: groupIds,
+                status: "ok"
+            });
+        });
+
+    } catch (err) {
+        res.json({
+            items: [],
+            status: err
+        });
+    }
 }
 
-exports.getGroups = function(req,res){
-   //console.log(req);
-    tradfri.getGroups().then(groups => {
-        res.json({items: groups, status:"ok"});
-    });
+exports.getGroups = function (req, res) {
+    try {
+        tradfri.getGroups().then(groups => {
+            res.json({
+                items: groups,
+                status: "ok"
+            });
+        });
+
+    } catch (err) {
+        res.json({
+            items: [],
+            status: err
+        });
+    }
 }
 
 
-exports.setDevice = function(req,res){
-    var q = {state: req.params.state};
+exports.setDevice = function (req, res) {
+    try {
+        var q = {
+            state: req.params.state
+        };
 
-    if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness<=255)
-        q.brightness = req.query.brightness;
+        if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness <= 255)
+            q.brightness = req.query.brightness;
 
-    tradfri.setDeviceState(req.params.deviceId, q).then(
-            res.json({status:"ok"})
+        tradfri.setDeviceState(req.params.deviceId, q).then(
+            res.json({
+                status: "ok"
+            })
         );
+
+    } catch (err) {
+        res.json({
+            items: [],
+            status: err
+        });
+    }
 }
 
-exports.setGroup = function(req,res){
-    var q = {state: req.params.state};
+exports.setGroup = function (req, res) {
+    try {
+        var q = {
+            state: req.params.state
+        };
 
-    if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness<=255)
-        q.brightness = req.query.brightness;
+        if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness <= 255)
+            q.brightness = req.query.brightness;
 
-    tradfri.setGroupState(req.params.groupId, q).then(
-            res.json({status:"ok"})
+        tradfri.setGroupState(req.params.groupId, q).then(
+            res.json({
+                status: "ok"
+            })
         );
+
+    } catch (err) {
+        res.json({
+            items: [],
+            status: err
+        });
+    }
 }
