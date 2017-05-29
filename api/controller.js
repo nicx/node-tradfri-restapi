f309c0fcae04092fe2f85b13cb59af96a8fa02d0
nidayand <pggithub@gothager.se>
2017-05-29 13:46:20 +0200

    getAll and fixing error handling

--- api/controller.js
+++ api/controller.js
@@ -8,115 +8,117 @@ const tradfri = require('node-tradfri-argon').create({
 });
 
 exports.getDeviceIds = function (req, res) {
-    try {
-        tradfri.getDeviceIds().then(deviceIds => {
-            res.json({
-                items: deviceIds,
-                status: "ok"
-            });
+    tradfri.getDeviceIds().then(deviceIds => {
+        res.json({
+            items: deviceIds,
+            status: "ok"
         });
-
-    } catch (err) {
+    }).catch((err) => {
         res.json({
             items: [],
             status: "err"
         });
-    }
+    });
+
 }
 
 exports.getDevices = function (req, res) {
-    try {
-        tradfri.getDevices().then(devices => {
-            res.json({
-                items: devices,
-                status: "ok"
-            });
+    tradfri.getDevices().then(devices => {
+        res.json({
+            items: devices,
+            status: "ok"
         });
-
-    } catch (err) {
+    }).catch((err) => {
         res.json({
             items: [],
             status: "err"
         });
-    }
+    });
+
 }
 exports.getGroupIds = function (req, res) {
-    try {
-        tradfri.getGroupIds().then(groupIds => {
-            res.json({
-                items: groupIds,
-                status: "ok"
-            });
+    tradfri.getGroupIds().then(groupIds => {
+        res.json({
+            items: groupIds,
+            status: "ok"
         });
-
-    } catch (err) {
+    }).catch((err) => {
         res.json({
             items: [],
             status: "err"
         });
-    }
+    });
+
 }
 
 exports.getGroups = function (req, res) {
-    try {
-        tradfri.getGroups().then(groups => {
-            res.json({
-                items: groups,
-                status: "ok"
-            });
+    tradfri.getGroups().then(groups => {
+        res.json({
+            items: groups,
+            status: "ok"
         });
-
-    } catch (err) {
+    }).catch((err) => {
         res.json({
             items: [],
             status: "err"
         });
-    }
+    });
+
+}
+
+exports.getAll = function (req, res) {
+    tradfri.getAll().then(groups => {
+        res.json({
+            items: groups,
+            status: "ok"
+        });
+    }).catch((err) => {
+        res.json({
+            items: [],
+            status: "err"
+        });
+    });
 }
 
 
 exports.setDevice = function (req, res) {
-    try {
-        var q = {
-            state: req.params.state
-        };
+    var q = {
+        state: req.params.state
+    };
 
-        if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness <= 255)
-            q.brightness = req.query.brightness;
+    if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness <= 255)
+        q.brightness = req.query.brightness;
 
-        tradfri.setDeviceState(req.params.deviceId, q).then(
-            res.json({
-                status: "ok"
-            })
-        );
-
-    } catch (err) {
+    tradfri.setDeviceState(req.params.deviceId, q).then(
+        res.json({
+            status: "ok"
+        })
+    ).catch((err) => {
         res.json({
             items: [],
             status: "err"
         });
-    }
+    });
+
 }
 
 exports.setGroup = function (req, res) {
-    try {
-        var q = {
-            state: req.params.state
-        };
+    var q = {
+        state: req.params.state
+    };
 
-        if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness <= 255)
-            q.brightness = req.query.brightness;
+    if (req.query.brightness && !isNaN(parseInt(req.query.brightness)) && req.query.brightness <= 255)
+        q.brightness = req.query.brightness;
 
-        tradfri.setGroupState(req.params.groupId, q).then(
-            res.json({
-                status: "ok"
-            })
-        );
-
-    } catch (err) {
+    tradfri.setGroupState(req.params.groupId, q).then(
+        res.json({
+            status: "ok"
+        })
+    ).catch((err) => {
         res.json({
             items: [],
             status: "err"
         });
-    }
+    });
+
 }
